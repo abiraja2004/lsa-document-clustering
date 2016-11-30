4ad2f3acbfa12c37623ac74b5dcf1ff083024b9b
xdrop <pastosdev@gmail.com>
2016-11-30 02:00:41 +0000

    Tidy up

--- main.py
+++ main.py
@@ -3,55 +3,55 @@ import filters
 from matrices import *
 from algorithms import LSA
 from normalizers import *
+from visualize import Visualize
 
 import nltk
 
 
-
-
-
 class Pipeline:
-
   """
   Class which assembles all the parts together to build a document clustering
   system.
   """
 
-  def __init__(self, docs):
+  def __init__(self):
     # set up some default filters
-    self.filterz = [filters.StopWordFilter("stopwords2.data"), filters.NLTKStopWordFilter(), filters.BasicWordFilter()]
-    # calculate the frequency matrix
-    self.term_matrix = FrequencyMatrix(docs, filters=self.filterz)
+    self.filterz = [filters.StopWordFilter("stopwords2.data"), filters.BasicWordFilter()]
 
-  def cluster_documents(self, dimensions_reduction, clusters, normalizer=TFIDF):
+  def cluster_documents(self, docs, dimensions_reduction, clusters, normalizer=TFIDF):
+    # calculate the frequency matrix
+    term_matrix = FrequencyMatrix(docs, filters=self.filterz)
     # Set-up Latent Semantic Analysis class
-    lsa = LSA(self.term_matrix, normalizer=TFIDF)
+    lsa = LSA(term_matrix, normalizer=TFIDF)
     # Decompose term matrix into SVD
     svd = lsa.decompose()
     # Reduce the dimensions of the SVD
     rsvd = lsa.reduce_svd(dimensions_reduction, svd)
-    ll = lsa.significant_terms(dimensions_reduction, svd)
-    best = sorted(ll, reverse=True)
-    print(best[0],best[1],best[2])
     # Cluster the data
     centroids, doc_clusters, labels = lsa.cluster(clusters, rsvd, dim=dimensions_reduction)
+    # Visualize it
     vis = Visualize()
     vis.plot_documents(rsvd, labels, doc_clusters, len(centroids))
     vis.show()
 
-  def summarize_sentences(self, dimensions_reduction, normalizer=TFIDF):
+  def summarize_sentences(self, sentences, dimensions_reduction, no_of_sentences=3, normalizer=TFIDF):
+    # calculate the frequency matrix
+    term_matrix = FrequencyMatrix(sentences, filters=self.filterz)
     # Set-up Latent Semantic Analysis class
-    lsa = LSA(self.term_matrix, normalizer=TFIDF)
+    lsa = LSA(term_matrix, normalizer=TFIDF)
     # Decompose matrix into SVD
     svd = lsa.decompose()
     # Calculate the most significant sentences based on SVD
     # more details see algorithms.py
-
-    pass
+    scores = lsa.significant_terms(dimensions_reduction, svd)
+    best = sorted(scores, reverse=True)
+    for i in range(0, no_of_sentences):
+      print(best[i][1], end=' ')
 
 
 def create_sentence_sources(path, tokenizer=None):
-  document = open(path).read()
+  """Loads up a document and splits it into sentences"""
+  document = open(path, encoding='utf-8').read()
   sources = []
   sentences = nltk.tokenize.sent_tokenize(document)
   i = 0
@@ -62,6 +62,7 @@ def create_sentence_sources(path, tokenizer=None):
 
 
 def create_document_sources(directory, tokenizer=None):
+  """Loads up the documents for clustering based on similarity"""
   docs = []
   for filename in os.listdir(directory):
     if filename.endswith(".txt"):
@@ -76,36 +77,45 @@ def input_or_default(default):
   else:
     return default
 
+
 ######################
 # Parameters section #
 ######################
 
 dir = "presidents_rivers"
-clusters = 3
+sum_file = "obama.txt"
+clusters = 4
 svd_dim = 2
-
-
-sentences = create_sentence_sources("presidents_rivers/bush.txt", tokenizer=langprocess.NLTKTokenizer)
-
-main = Pipeline(sentences)
-main.run(svd_dim, clusters)
-pass
 
 # tokenizer which breaks words down
 tokenizer = langprocess.NLTKTokenizer
 # normalizer of frequency matrix
-normalizer=TFIDF
+normalizer = TFIDF
 
+main = Pipeline()
+
+print("------------------------------")
+print("Text summarization example\n")
+print("Enter file to summarize\n (Press enter for default: " + sum_file + ")")
+sum_file = input_or_default(sum_file)
+
+sentences = create_sentence_sources(os.path.join(dir, sum_file), tokenizer=langprocess.NLTKTokenizer)
+
+print("Summary of", sum_file)
+main.summarize_sentences(sentences, 10)
+print("")
+
+print("------------------------------------")
+print("Document clustering example\n")
 print("Enter directory to load examples from\n(Press enter for default: " + dir + ")")
 
 dir = input_or_default(dir)
 
-docs=create_document_sources(dir, tokenizer=langprocess.NLTKTokenizer)
+docs = create_document_sources(dir, tokenizer=langprocess.NLTKTokenizer)
 print("Enter the number of clusters to detect: \n(Press enter for default: " + str(clusters) + ")")
 clusters = int(input_or_default(clusters))
 
 print("Enter SVD dimensions for clustering: \n(Press enter for default: " + str(svd_dim) + ")")
 svd_dim = int(input_or_default(svd_dim))
 
-main = Pipeline(docs)
-main.run(svd_dim, clusters)
+main.cluster_documents(docs, svd_dim, clusters)
